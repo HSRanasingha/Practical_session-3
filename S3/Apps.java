1837c4743af995b509af4bc749a0e27b0eddcd9d
H S Ranasingha <harshanisadunika99@gmail.com>
2025-03-21 17:09:22 +0530

    Practical_session 3
    Working with Packages in Java

---
+++ S3/Apps.java
@@ -0,0 +1,12 @@
+import a.*;
+import b.*;
+
+class Apps{
+     public static void main(String[] args){
+		 a.Test t1=new a.Test();
+		 b.Test t2=new b.Test();
+		 t1.display();
+		 t2.print();
+	 } 
+
+}
